5548690f4a9f45c2d276bf36cfa8ca325a71dfc0
Christian Kamm <christian.d.kamm@nokia.com>
2010-03-23 12:57:51 +0100

    Add more complex test.

--- tests/auto/basic/tst_basic.cpp
+++ tests/auto/basic/tst_basic.cpp
@@ -7,6 +7,7 @@ Q_OBJECT
 
 private slots:
     void noYield();
+    void coroutineStartingCoroutine();
 };
 
 class NoYieldCoro : public Coroutine
@@ -31,6 +32,87 @@ void tst_basic::noYield()
     QCOMPARE(coro.i, 1);
     QCOMPARE(coro.status(), Coroutine::Terminated);
     Q_ASSERT(Coroutine::currentCoroutine() != &coro);
+}
+
+namespace CoroutineStartingCoroutine {
+    static Coroutine *base = 0;
+    static Coroutine *c1 = 0;
+
+    static void c1code()
+    {
+        QCOMPARE(base->status(), Coroutine::Running);
+        QCOMPARE(c1->status(), Coroutine::Running);
+        QCOMPARE(Coroutine::currentCoroutine(), c1);
+
+        Coroutine::yield();
+
+        QCOMPARE(base->status(), Coroutine::Stopped);
+        QCOMPARE(c1->status(), Coroutine::Running);
+        QCOMPARE(Coroutine::currentCoroutine(), c1);
+    }
+
+    static void startCoroutines()
+    {
+        QCOMPARE(base->status(), Coroutine::Running);
+        QCOMPARE(Coroutine::currentCoroutine(), base);
+
+        c1 = Coroutine::build(&c1code);
+        Q_ASSERT(c1);
+        QCOMPARE(c1->status(), Coroutine::NotStarted);
+
+        Coroutine::yield();
+
+        QCOMPARE(base->status(), Coroutine::Running);
+        QCOMPARE(c1->status(), Coroutine::NotStarted);
+        QCOMPARE(Coroutine::currentCoroutine(), base);
+
+        QCOMPARE(c1->cont(), true);
+
+        QCOMPARE(base->status(), Coroutine::Running);
+        QCOMPARE(c1->status(), Coroutine::Stopped);
+        QCOMPARE(Coroutine::currentCoroutine(), base);
+
+        Coroutine::yield();
+
+        QCOMPARE(base->status(), Coroutine::Running);
+        QCOMPARE(c1->status(), Coroutine::Terminated);
+        QCOMPARE(Coroutine::currentCoroutine(), base);
+    }
+}
+
+void tst_basic::coroutineStartingCoroutine()
+{
+    using namespace CoroutineStartingCoroutine;
+
+    Coroutine *start = Coroutine::currentCoroutine();
+
+    base = Coroutine::build(&startCoroutines);
+    Q_ASSERT(base);
+
+    QCOMPARE(base->cont(), true);
+
+    Q_ASSERT(c1);
+    QCOMPARE(base->status(), Coroutine::Stopped);
+    QCOMPARE(c1->status(), Coroutine::NotStarted);
+    QCOMPARE(Coroutine::currentCoroutine(), start);
+
+    QCOMPARE(base->cont(), true);
+
+    QCOMPARE(base->status(), Coroutine::Stopped);
+    QCOMPARE(c1->status(), Coroutine::Stopped);
+    QCOMPARE(Coroutine::currentCoroutine(), start);
+
+    QCOMPARE(c1->cont(), false);
+
+    QCOMPARE(base->status(), Coroutine::Stopped);
+    QCOMPARE(c1->status(), Coroutine::Terminated);
+    QCOMPARE(Coroutine::currentCoroutine(), start);
+
+    QCOMPARE(base->cont(), false);
+
+    QCOMPARE(base->status(), Coroutine::Terminated);
+    QCOMPARE(c1->status(), Coroutine::Terminated);
+    QCOMPARE(Coroutine::currentCoroutine(), start);
 }
 
 QTEST_MAIN(tst_basic)
